22768750a5f5124e0bb2f4534cbd8bd286cd7a06
Blisstod <onetenge@gmail.com>
2023-09-26 14:58:07 +0600

    observer

--- observer.go
+++ observer.go
@@ -1,3 +1,51 @@
 package main
 
-//a;slkdjf;laksdfj
+import "fmt"
+
+type Observer interface {
+	update(string)
+}
+
+type Observable interface {
+	subscribe(observer Observer)
+	notifyAll()
+}
+
+type User struct {
+	id string
+}
+
+type Streamer struct {
+	observerList []Observer
+	name         string
+}
+
+func (u *User) update(streamerName string) {
+	fmt.Printf("%s has started the stream\n", streamerName)
+}
+
+func newStreamer(name string) *Streamer {
+	return &Streamer{name: name}
+}
+
+func (s *Streamer) subscribe(o Observer) {
+	s.observerList = append(s.observerList, o)
+}
+
+func (s *Streamer) notifyAll() {
+	for _, observer := range s.observerList {
+		observer.update(s.name)
+	}
+}
+
+func main() {
+	Bratishkinoff := newStreamer("Bratishkinoff")
+
+	blisstod := &User{id: "blisstod@gmail.com"}
+	skits := &User{id: "skits@gmail.com"}
+
+	Bratishkinoff.subscribe(blisstod)
+	Bratishkinoff.subscribe(skits)
+
+	Bratishkinoff.notifyAll()
+}
